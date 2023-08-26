82c4312fa9d55c87b303b762a307777433f216a3
aamiraminali <aamir.amin@logicielservice.com>
2023-08-26 16:21:47 +0200

    Creating NetService Creating NetServiceTests

---
+++ NetUtility/Services/NetService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace NetUtility.Services
+{
+	public class NetServices
+	{
+		public NetServices()
+		{
+		}
+
+		public string Ping()
+		{
+			return "192.168.2.123 Ping!";
+		}
+	}
+}
+
---
+++ NetUtilityTests/Tests/NetServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace NetUtilityTests.Tests
+{
+	public class NetServiceTests
+	{
+		public NetServiceTests()
+		{
+		}
+
+        [Fact]
+        public void NetServices_Ping_ReturnString()
+        {
+
+        }
+    }
+}
+
