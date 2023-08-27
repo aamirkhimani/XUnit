60d7b2377d5a8abb46ba329fdbf17d2a6f3a7513
aamiraminali <aamir.amin@logicielservice.com>
2023-08-27 16:01:59 +0200

    Adding ClubRepositoryTests

---
+++ RunGroopWebAppTests/Repository/ClubRepositoryTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Data.Enum;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+
+namespace RunGroopWebAppTests.Repository
+{
+    public class ClubRepositoryTests
+    {
+
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var appDbContext = new ApplicationDbContext(options);
+            appDbContext.Database.EnsureCreated();
+
+            if (await appDbContext.Clubs.CountAsync() < 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    appDbContext.Clubs.Add(
+                        new Club()
+                        {
+                            Title = $"Running Club {i+1}",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Description = "This is the description of the first cinema",
+                            ClubCategory = ClubCategory.City,
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Charlotte",
+                                State = "NC"
+                            }
+                        }
+                        );
+
+                    await appDbContext.SaveChangesAsync();
+                }
+            }
+
+            return appDbContext;
+        }
+
+        [Fact]
+        public async void ClubRepository_Add_Returnsbool()
+        {
+            //Arrange
+            var club = new Club()
+            {
+                Title = "Running Club 1",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first cinema",
+                ClubCategory = ClubCategory.City,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Charlotte",
+                    State = "NC"
+                }
+            };
+
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            var result = clubRepository.Add(club);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async void ClubRepository_GetByIdAsync_ReturnsClubObject()
+        {
+            //Arrange
+            int id = 1;
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            var result = clubRepository.GetByIdAsync(id);
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
+
